17d251d87d335196a918b81f508b7244b58d9056
Umar Baymran <1000slow@gmail.com>
2023-08-01 06:59:54 +0300

    fixup! feat(app): add data dumps

--- docs/docs/settings.py
+++ docs/docs/settings.py
@@ -77,9 +77,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'charset': 'windows-1251',
-        },
     }
 }
 
